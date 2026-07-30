c50d2f832c4bd39ff555a93a8fd33e607421367c
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-07-30 22:53:17 +0330

    Add Shared Layer

---
+++ Shared/ErrorMessages.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Shared;
+
+public static class ErrorMessages
+{
+    public const string NotAdded = "اضافه نشد!";
+    public const string NotUpdated = "ویرایش نشد!";
+    public const string NotDeleted = "حذف نشد!";
+    public const string NotFound = "یافت نشد!";
+    public const string PasswordDoNotMatch = "رمز عبور و تایید رمز عبور مطابقت ندارد!";
+    public const string IsExistEmail = "این ایمیل از قبل وجود دارد!";
+    public const string IsExistUserName = "این نام کاربری از قبل وجود دارد!";
+    public const string IsExistUser = "این کاربر از قبل وجود دارد";
+    public const string UserNotFound = "کاربر یافت نشد";
+}
---
+++ Shared/GeneralResponse/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace Shared;
+
+public class Result<T>
+{
+    public string Message { get; set; }
+    public bool Success { get; set; }
+    public T? Data { get; set; }
+
+    public static Result<T> SuccessRes(T data)
+    {
+        return new Result<T>(data);
+    }
+
+    private Result(T data)
+    {
+        Success = true;
+        Data = data;
+        Message = "عملیات موفقیت آمیز بود";
+    }
+
+    public static Result<T> FailRes(string message = "")
+    {
+        if (string.IsNullOrEmpty(message))
+            return new Result<T>("عملیات شکست خورد");
+
+        return new Result<T>(message);
+    }
+
+    private Result(string message)
+    {
+        Success = false;
+        Message = message;
+    }
+}
