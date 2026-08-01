cc07e087e9e08ed42c669fc2035adb48dbc02c24
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-08-01 20:19:32 +0330

    Add GetIdentityErrorMessage method In identity

---
+++ Application/Commons/IdentityExtentions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Application.Commons;
+
+public static class IdentityExtentions
+{
+    public static string GetIdentityErrorMessage(this IdentityResult identityResult)
+    {
+        return string.Join(" , ", identityResult.Errors.Select(i => $"{i.Code}: {i.Description}"));
+    }
+}
--- Application/GlobalUsing.cs
+++ Application/GlobalUsing.cs
@@ -1,1 +1,2 @@
 ﻿global using Shared;
+global using Application.Commons;
