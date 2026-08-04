ab1f61b37d504ccc7a06ce8b3fbaec7f70bb97e9
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-08-04 12:39:49 +0330

    fix

--- Application/Features/UserFeature/ViewModels/Responses/User_GetCurrentUser_Response.cs
+++ Application/Features/UserFeature/ViewModels/Responses/User_GetCurrentUser_Response.cs
@@ -6,5 +6,5 @@ public class User_GetCurrentUser_Response
     public string LastName { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty; 
     public string? Email { get; set; }
-    public IList<string> Roles { get; set; }
+    public IList<string> Roles { get; set; } = new List<string>();
 }
