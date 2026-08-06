ed4a61af7e0db2b2a0cb9f38061ed4cc7e3703b9
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-08-06 16:10:10 +0330

    Add general Pageination

---
+++ Infrastructure/Common/Pageination.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commons;
+
+namespace Infrastructure.Common;
+
+public static class Pageination
+{
+    public static IQueryable<TEntity> UsePageination<TEntity, TRequest>(this IQueryable<TEntity> query, TRequest request)
+        where TEntity : class
+        where TRequest : PagerViewModel
+    {
+        if (request.PageNo > 0)
+            request.PageNo = 0;
+
+        if (request.PageSize > 0)
+            request.PageSize = 0;
+
+        if (request.PageNo != 0 && request.PageSize != 0)
+            query = query.Skip((request.PageNo - 1) * request.PageSize).Take(request.PageSize);
+
+        return query;
+    }
+}
