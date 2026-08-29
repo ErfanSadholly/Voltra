e2653377b2f7b0afa119d13c6a0e13cce01f1c91
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-08-29 22:31:57 +0330

    fix

--- Src/Api/Controllers/AuthController.cs
+++ Src/Api/Controllers/AuthController.cs
@@ -1,44 +1,46 @@
 ﻿using Application.Features;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BattryShopApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : CommonController
-    {
-        private readonly IAuthFeature _feature;
-        public AuthController(IAuthFeature authFeature)
-        {
-            _feature = authFeature;
-        }
+	[AllowAnonymous]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : CommonController
+	{
+		private readonly IAuthFeature _feature;
+		public AuthController(IAuthFeature authFeature)
+		{
+			_feature = authFeature;
+		}
 
-        [HttpPost("[action]")]
-        public async Task<IActionResult> Register([FromBody] Auth_Register_Request request)
-        {
-            var res = await _feature.Register(request);
-            return Ok(res);
-        }
+		[HttpPost("[action]")]
+		public async Task<IActionResult> Register([FromBody] Auth_Register_Request request)
+		{
+			var res = await _feature.Register(request);
+			return Ok(res);
+		}
 
-        [HttpPost("[action]")]
-        public async Task<IActionResult> Login([FromBody] Auth_Login_Request request)
-        {
-            var res = await _feature.Login(request);
-            return Ok(res);
-        }
+		[HttpPost("[action]")]
+		public async Task<IActionResult> Login([FromBody] Auth_Login_Request request)
+		{
+			var res = await _feature.Login(request);
+			return Ok(res);
+		}
 
-        [HttpPost("[action]")]
-        public async Task<IActionResult> RefreshToken([FromBody] string token)
-        {
-            var res = await _feature.RefreshToken(token);
-            return Ok(res);
-        }
+		[HttpPost("[action]")]
+		public async Task<IActionResult> RefreshToken([FromBody] string token)
+		{
+			var res = await _feature.RefreshToken(token);
+			return Ok(res);
+		}
 
-        [HttpPost("[action]")]
-        public async Task<IActionResult> Logout([FromBody] string token)
-        {
-            var res = await _feature.Logout(token);
-            return Ok(res);
-        }
-    }
+		[HttpPost("[action]")]
+		public async Task<IActionResult> Logout([FromBody] string token)
+		{
+			var res = await _feature.Logout(token);
+			return Ok(res);
+		}
+	}
 }
