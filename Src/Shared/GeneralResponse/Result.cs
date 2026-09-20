079794ebef0f218aad687de0c6bdd9f2a5fa0b15
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-09-20 13:24:56 +0330

    add Code in response

--- Src/Shared/GeneralResponse/Result.cs
+++ Src/Shared/GeneralResponse/Result.cs
@@ -1,32 +1,40 @@
-﻿namespace Shared;
+﻿using System.Runtime.CompilerServices;
+
+namespace Shared;
 
 public class Result<T> : GeneralResponse
 {
-    public T? Data { get; set; }
+	public T? Data { get; set; }
+	public string? Code { get; set; }
 
-    public static Result<T> SuccessRes(T data)
-    {
-        return new Result<T>(data);
-    }
+	public static Result<T> SuccessRes(T data)
+	{
+		return new Result<T>(data);
+	}
 
-    private Result(T data)
-    {
-        Success = true;
-        Data = data;
-        Message = "عملیات موفقیت آمیز بود";
-    }
+	private Result(T data)
+	{
+		Success = true;
+		Data = data;
+		Message = "عملیات موفقیت آمیز بود";
+	}
 
-    public static Result<T> FailRes(string message = "")
-    {
-        if (string.IsNullOrEmpty(message))
-            return new Result<T>("عملیات شکست خورد");
+	public static Result<T> FailRes(string message = "", [CallerArgumentExpression("message")] string? expression = null)
+	{
+		if (string.IsNullOrEmpty(message))
+			return new Result<T>("عملیات شکست خورد" , "Failed");
 
-        return new Result<T>(message);
-    }
+		string? code = null;
+		if (expression?.StartsWith("ErrorMessages.") == true)
+			code = expression.Split('.').Last();
 
-    private Result(string message)
-    {
-        Success = false;
-        Message = message;
-    }
+		return new Result<T>(message , code);
+	}
+
+	private Result(string message , string? code)
+	{
+		Success = false;
+		Message = message;
+		Code = code;
+	}
 }
